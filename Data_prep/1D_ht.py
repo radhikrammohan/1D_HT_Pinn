aff37fd444a9d7b7bcf1ea7245a2be049fec1fed
radhikrammohan <radhikrammohan@gmail.com>
2023-11-29 15:22:33 +1100

    rr

--- Data_prep/1D_ht.py
+++ Data_prep/1D_ht.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt  # import matplotlib.pyplot library as plt
 import csv # import csv module
 
 # Parameters
-alpha = 0.01  # Thermal diffusivity
+alpha = 0.1  # Thermal diffusivity
 length = 1.0  # Length of the rod
 time_end = 1.0  # End time
 num_points = 100  # Number of spatial points
-num_steps = 1000  # Number of time steps
+num_steps = 10000  # Number of time steps
 
 # Spatial and time step sizes
 dx = length / (num_points - 1) # Grid spacing
@@ -17,7 +17,7 @@ dt = time_end / num_steps # Time step size
 initial_temperature = np.zeros(num_points) # Initialize temperature array with zeros
 
 # Set one end at 25 degrees and the other end at 600 degrees
-initial_temperature[0] = 100.0  # Set the first element (one end) to 25 degrees
+initial_temperature[0:-1] = 100.0  # Set the first element (one end) to 25 degrees
 initial_temperature[-1] = 600.0  # Set the last element (other end) to 600 degrees
 
 # Initialize temperature array
@@ -28,9 +28,9 @@ for n in range(1, num_steps + 1):# Loop through time steps
     # Compute new temperature values using finite difference
     temperature[1:-1] = (
         temperature[1:-1]
-        + alpha * dt / dx**2 * (temperature[2:] - 2 * temperature[1:-1] + temperature[:-2])
+        + alpha * (dt / dx**2) * (temperature[2:] - 2 * temperature[1:-1] + temperature[:-2])
     )
-
+print(temperature)
 # Plot the results
 plt.plot(np.linspace(0, length, num_points), temperature)
 plt.xlabel('Spatial Coordinate (x)')
