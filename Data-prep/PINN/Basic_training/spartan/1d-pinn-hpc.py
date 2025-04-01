8581875e298d09ea118010d16290b150a1cea3cc
radhikrammohan <radhikrammohan@gmail.com>
2025-04-02 10:40:29 +1100

    bad data

--- Data-prep/PINN/Basic_training/spartan/1d-pinn-hpc.py
+++ Data-prep/PINN/Basic_training/spartan/1d-pinn-hpc.py
@@ -309,7 +309,7 @@ output_size=1
 learning_rate = 0.005
 hidden_layers = 5
 
-epochs_1 = 20000
+epochs_1 = 2000
 epochs_2 = 10
 
 model = PINN(input_size, hidden_size, output_size,hidden_layers).to(device)
